067eb98e69f14bfcef5f3c2357f29f26aad6444c
Joaquin Orma <jormachea@itba.edu.ar>
2020-07-05 14:11:26 -0300

    React Structure Updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Popoto from './Components/Popoto';
+import {Navbar, Nav, Container, Col, Row} from 'react-bootstrap';
 import NeoGraph from "./Components/NeoGraph";
-import { render } from '@testing-library/react';
 
 const NEO4J_URI = "bolt://localhost:7687";
 const NEO4J_USER = "neo4j";
@@ -13,42 +11,33 @@ const NEO4J_PASSWORD = "admin";
 class App extends Component {
   render() {
     return (
-      <div className="container h-100">
-        <div className="row">
-          <h1>React Neovis Example</h1>
-        </div>
-        <div className="App row h-100" style={{ fontFamily: "Quicksand" }}>
-          <NeoGraph
-            width={400}
-            height={700}
-            containerId={"id1"}
-            neo4jUri={NEO4J_URI}
-            neo4jUser={NEO4J_USER}
-            neo4jPassword={NEO4J_PASSWORD}
-            backgroundColor={"#b2beb5"}
-          />
-        </div>
-      </div>
-      /*
-      <Popoto />
-      /*
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-      */
+      <Container>
+        <Row className="pb-1 pt-1">
+          <Col>
+            <Navbar bg="primary" variant="dark" className="justify-content-between">
+              <Navbar.Brand href="#home">React NeoVis Example</Navbar.Brand>
+              <Nav className="ml-auto">
+                <Nav.Link href="#home">Graph Viz</Nav.Link>
+                <Nav.Link href="#features">Stats</Nav.Link>
+                <Nav.Link href="#pricing">Info</Nav.Link>
+              </Nav>
+            </Navbar>
+          </Col>
+        </Row>
+        <Row >
+          <Col>
+            <NeoGraph
+              width={400}
+              height={700}
+              containerId={"id1"}
+              neo4jUri={NEO4J_URI}
+              neo4jUser={NEO4J_USER}
+              neo4jPassword={NEO4J_PASSWORD}
+              backgroundColor={"rgba(178, 190, 181, 0.16)"}
+            />
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
