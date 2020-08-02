39707277c5d92cd2d7fcac90e201b4a3a09925a9
Joaquin Orma <jormachea@itba.edu.ar>
2020-08-02 19:52:02 -0300

    Prueba con VX || Commented api_cypherQuery

--- src/App.js
+++ src/App.js
@@ -3,13 +3,145 @@ import './App.css';
 import {Navbar, Nav, Container, Col, Row} from 'react-bootstrap';
 import NeoGraph from "./Components/NeoGraph";
 import TempSlider from "./Components/TempSlider";
+import { Graph } from "@vx/network";
+import {
+  forceCenter,
+  forceLink,
+  forceManyBody,
+  forceSimulation
+} from "d3-force";
+import {api_cypherQuery} from './GraphService/graphQueryService';
 
 const NEO4J_URI = "bolt://localhost:7687";
 const NEO4J_USER = "neo4j";
 const NEO4J_PASSWORD = "admin";
 
+class NetworkNode extends React.Component {
+  render() {
+    return <circle r={10} fill={"#9280FF"} />;
+  }
+}
+
+class Network extends React.Component {
+  constructor(props) {
+    super(props);
+
+    const links = props.network.links;
+    const nodes = props.network.nodes;
+
+    this.state = {
+      data: {
+        nodes,
+        links
+      }
+    };
+    this.networkCallback = this.networkCallback.bind(this);
+  }
+
+  // Update force if the width or height of the graph changes
+  componentDidUpdate(newProps) {
+    this.force = this.force
+      .force("center", forceCenter(
+        newProps.width / 2,
+        newProps.height / 2
+      ))
+      .restart();
+  }
+
+  // Setup D3 force
+  componentDidMount() {
+    this.force = forceSimulation(this.state.data.nodes)
+      .force(
+        "link",
+        forceLink()
+          .id(function(d) {
+            return d.id;
+          })
+          .links(this.state.data.links)
+      )
+      .force("charge", forceManyBody().strength(-500))
+      .force(
+        "center",
+        forceCenter(this.props.width / 2, this.props.height / 2)
+      );
+
+    // Force-update the component on each force tick
+    this.force.on("tick", () => this.forceUpdate());
+    //api_newCypher(this.networkCallback);
+    //api_cypherQuery("match (n) with collect(id(n)) as nodes match (m)-[r]->(o) with nodes, collect([id(m),id(o)]) as edges return nodes, edges", this.networkCallback);
+  }
+
+  networkCallback(response){
+    console.log("LLEGUE")
+    let response_tuple = response.results[0].data[0].row
+    let nodes = [];
+    let links = [];
+    response_tuple[0].forEach(e => {
+      nodes.push({id: e})
+    });
+    response_tuple[1].forEach(e => {
+      links.push({source: e[0], target: e[1]})
+    });
+    this.setState({
+      data: {
+        nodes: nodes,
+        links: links
+      }
+    });
+  }
+
+  // networkCallback2(response){
+  //     let dataArray = response.results[0].data;
+  //     //console.log(dataArray)
+  //     let nodes = [];
+  //     let links = [];
+  //     dataArray.forEach(e => {
+  //         nodes.push(e.row[0]);
+  //         links.push(e.meta[2]);
+  //     });
+  //     //console.log(nodes);
+  //     //console.log(links);
+  //     this.setState({ results: response});
+  // }
+
+  render() {
+    if (!this.force) {
+      return null;
+    }
+
+    return (
+      <div style={{ width: "100%", height: "100%" }}>
+        <svg width={this.props.width} height={this.props.height}>
+          <rect
+            width={this.props.width}
+            height={this.props.height}
+            fill="#f9fcff"
+          />
+          <Graph graph={this.state.data} nodeComponent={NetworkNode} />
+        </svg>
+      </div>
+    );
+  }
+}
+
+const nodes = [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 }];
+const links = [
+  { source: 1, target: 2 },
+  { source: 1, target: 3 },
+  { source: 1, target: 4 },
+  { source: 2, target: 4 },
+  { source: 3, target: 4 },
+  { source: 4, target: 5 }
+];
 
 class App extends Component {
+  componentDidMount() {
+    
+  }
+
+  componentWillUnmount() {
+  }
+
   render() {
     return (
       <Container>
@@ -27,7 +159,15 @@ class App extends Component {
         </Row>
         <Row >
           <Col>
-            <NeoGraph
+            <Network
+              width={400}
+              height={400}
+              network={{
+                nodes: nodes,
+                links: links
+              }}
+            />
+            {/* <NeoGraph
               width={400}
               height={675}
               containerId={"id1"}
@@ -35,7 +175,7 @@ class App extends Component {
               neo4jUser={NEO4J_USER}
               neo4jPassword={NEO4J_PASSWORD}
               backgroundColor={"rgba(178, 190, 181, 0.16)"}
-            />
+            /> */}
           </Col>
         </Row>
         <TempSlider initMinDate={1900} initMaxDate={1980}/>
--- src/GraphService/graphQueryService.js
+++ src/GraphService/graphQueryService.js
@@ -51,3 +51,28 @@ export function api_getNodesAndEdges(callback){
         statements:[{statement:query,parameters:params}]
         }));
 }
+
+export function api_cypherQuery(queryParam, callback){
+    let query = queryParam;
+    let params = {};
+    let xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function() {
+        if(this.readyState !==4){
+            return;
+        }
+        if (this.status === 200) {
+            let results = JSON.parse(this.responseText);
+            callback(results);
+        } else {
+            callback(null);
+        }
+    };
+    xhttp.open("POST", txUrl, true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.setRequestHeader("Authorization", "Basic "+ btoa(NEO_USER + ':' + NEO_PASS));
+    xhttp.setRequestHeader("Data-type", "json");
+    xhttp.send(JSON.stringify({
+        statements:[{statement:query,parameters:params}]
+        }));
+}
