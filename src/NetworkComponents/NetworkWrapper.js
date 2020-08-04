ab57e2f672a22450d88423553e4d2d987a89a007
Diego Orlando <diego.orlando@accendoit.com>
2020-08-04 01:53:37 -0300

    Need help with maps (read comments)

--- src/NetworkComponents/NetworkWrapper.js
+++ src/NetworkComponents/NetworkWrapper.js
@@ -3,15 +3,9 @@ import Network from './Network';
 import { api_cypherQuery } from '../GraphService/graphQueryService';
 
 //MockValues
-const nodesMock = [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 }];
-const linksMock = [
-  { source: 1, target: 2 },
-  { source: 1, target: 3 },
-  { source: 1, target: 4 },
-  { source: 2, target: 4 },
-  { source: 3, target: 4 },
-  { source: 4, target: 5 }
-];
+const nodesMock = [];
+const linksMock = [];
+const attrsMock = [];
 
 class NetworkWrapper extends React.Component {
     constructor(props){
@@ -20,13 +14,19 @@ class NetworkWrapper extends React.Component {
             data: {
                 nodes: nodesMock,
                 links: linksMock
-            }
+            },
+            attrs: attrsMock
         }
         this.networkCallback = this.networkCallback.bind(this);
+        this.infoCallback = this.infoCallback.bind(this);
     }
 
     componentDidMount(){
-        api_cypherQuery("match (n) with collect(id(n)) as nodes match (m)-[r]->(o) with nodes, collect([id(m),id(o)]) as edges return nodes, edges", this.networkCallback);
+        api_cypherQuery("match (n) with collect([id(n),n]) as nodes match (m)-[r]->(o) with nodes, collect([[id(m),id(o)],r]) as edges return nodes, edges", this.networkCallback);
+        api_cypherQuery("match (o:Object)-->(a:Attribute)-->(v:Value) return id(o), o.title, a.title, v.value order by id(o)", this.infoCallback);
+        //Aca deberia setearle en el name del node, el value del atributo sacado del map
+        //pero no se como hacer un mapa en esto de react. Osea el loop del networkCallback
+        //va aca.        
     }
     
     nodeHoverTooltip(node){
@@ -36,14 +36,16 @@ class NetworkWrapper extends React.Component {
     }
 
     networkCallback(response){
-        let response_tuple = response.results[0].data[0].row
+        let response_tuple = response.results[0].data[0].row;
         let nodes = [];
         let links = [];
+        //No deberiamos loopear aca, sino en el componentDidMount para poder asignarle
+        //los atributos al nodo. Aca solo guardamos los nodos como estan.
         response_tuple[0].forEach(e => {
-            nodes.push({id: e, name: e})
+            nodes.push({id: e[0], name: e[1].title || e[1].value})
         });
         response_tuple[1].forEach(e => {
-            links.push({source: e[0], target: e[1]})
+            links.push({source: e[0][0], target: e[0][1]})
         });
         this.setState({
             data: {
@@ -51,7 +53,28 @@ class NetworkWrapper extends React.Component {
                 links: links
             }
         });
-        //this.updateForceSimulation();
+    }
+
+    infoCallback(response){
+        //El mapa deberia ser con key el id del nodo, y el value deberian ser cada
+        //cada uno de los atributos del nodo. Es decir tendria [key,value] adentro,
+        //donde key es el nombre del atributo, y value es el valor.
+        
+        /*
+        let response_table = response.results[0].data;
+        const attrs = response_table.map((attr) =>
+            <Post
+                key={attr.row[0]}
+                id={attr.row[0]}
+                title={attr.row[1]}
+                attribute={attr.row[2]}
+                value={attr.row[3]}
+            />
+            );
+        this.setState({
+            attrs: attrs
+        });
+        */
     }
 
     render(){
