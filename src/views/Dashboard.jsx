b9bc5fab14ebefa6ec1b26e961ad58efb7c79928
Joaquin Orma <jormachea@itba.edu.ar>
2020-08-17 20:23:16 -0300

    No Luck

--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -30,10 +30,36 @@ import {
   legendBar,
   legendNodes
 } from "variables/Variables.jsx";
+import TryVis from "components/NetworkComponents/TryVis"
+import {MyVis} from "components/NetworkComponents/MyVis"
 import NetworkVis from "components/NetworkComponents/NetworkVis"
 import FilterModule from "components/NetworkComponents/FilterModule";
 import NodeVisualizer from "components/NetworkComponents/NodeVisualizer";
 import { neo4j_config } from "variables/ConnectionVariables";
+import { DataSet, Network } from 'vis-network/standalone';
+
+
+const nodes = new DataSet([
+  { id: 1, label: 'Node 1' },
+  { id: 2, label: 'Node 2' },
+  { id: 3, label: 'Node 3' },
+  { id: 4, label: 'Node 4' },
+  { id: 5, label: 'Node 5' }
+]);
+
+// create an array with edges
+const edges = new DataSet([
+  { from: 1, to: 3 },
+  { from: 1, to: 2 },
+  { from: 2, to: 4 },
+  { from: 2, to: 5 }
+]);
+
+const my_data = {
+  nodes: nodes,
+  edges: edges
+};
+
 
 class Dashboard extends Component {
   constructor(props){
@@ -89,7 +115,9 @@ class Dashboard extends Component {
         ;
         NetworkCardlegend = '';
     } else {
-      NetworkCardContent = <NetworkVis con_config={ this.props.connection.neo4j_config } visual={this.props.visual}/>;
+      NetworkCardContent = <TryVis con_config={ this.props.connection.neo4j_config }/>
+      // NetworkCardContent = <MyVis con_config={ this.props.connection.neo4j_config } visual={this.props.visual} data={my_data}/>;
+      // NetworkCardContent = <NetworkVis con_config={ this.props.connection.neo4j_config } visual={this.props.visual}/>;
       // ToDo: revisar legend
       NetworkCardlegend = this.createLegend(this.props.visual);
       // NetworkCardlegend = '';
